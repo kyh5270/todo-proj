e5c6cc6b73604e1b6b5771779fd0009fd4643b29
권용호 <kyh5270@gmail.com>
2021-02-27 22:31:27 +0900

    ~ [ TypeScript ] Step 4-2 User Interaction(CLI)

---
+++ build/service/TodoCollection.js
@@ -0,0 +1,50 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const TodoItem_1 = __importDefault(require("../model/TodoItem"));
+class TodoCollection {
+    constructor(userName, todoItems = []) {
+        this.userName = userName;
+        this.todoItems = todoItems;
+        this.nextId = 1;
+        this.itemMap = new Map();
+        todoItems.forEach((item) => this.itemMap.set(item.id, item));
+    }
+    getTodoById(id) {
+        return this.itemMap.get(id);
+    }
+    addTodo(task) {
+        while (this.getTodoById(this.nextId)) {
+            this.nextId++;
+        }
+        this.itemMap.set(this.nextId, new TodoItem_1.default(this.nextId, task));
+        return this.nextId;
+    }
+    // includeComplete is true > return all todolist
+    // includeComplete is false > return not complete todolist
+    getTodoItems(includeComplete) {
+        return [...this.itemMap.values()].filter((item) => includeComplete || !item.complete);
+    }
+    removeComplete() {
+        this.itemMap.forEach((item) => {
+            if (item.complete) {
+                this.itemMap.delete(item.id);
+            }
+        });
+    }
+    getItemCounts() {
+        return {
+            total: this.itemMap.size,
+            incomplete: this.getTodoItems(false).length,
+        };
+    }
+    markComplete(id, complete) {
+        const todoItem = this.getTodoById(id);
+        if (todoItem) {
+            todoItem.complete = complete;
+        }
+    }
+}
+exports.default = TodoCollection;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-import TodoItem from './TodoItem';
-import { data } from './data';
-import TodoCollection from './TodoCollection';
+import TodoConsole from "./view/TodoCosole";
 
-const sampleTodos : TodoItem [] = data.map(
-    (item) => new TodoItem(item.id, item.task, item.complete)
-);
-
-const myTodoCollection = new TodoCollection("My Sample Todos", sampleTodos);
-
-myTodoCollection.addTodo("JavaScript 학습하기");
-myTodoCollection.addTodo("친구 만나기");
-
-myTodoCollection.markComplete(3, true);
-
-console.log(`${myTodoCollection.userName}`);
-
-myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
-console.log("=======");
-myTodoCollection.removeComplete();
-myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+const todoConsole = new TodoConsole();
+todoConsole.promptUser();
